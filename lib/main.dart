e6088aa7c6c3bf27a268ee734c031ae3c685419e
abhsurek <abhsurek@cisco.com>
2021-04-29 00:33:57 +0530

    test

--- lib/main.dart
+++ lib/main.dart
@@ -9,17 +9,24 @@ void main()
 
 class OxyApp extends StatelessWidget //  every widget has to extend either of the two: StatelessWidget or StatefulWidget
 {
+  int listIndex = 0;
+  void viewDetails() {
+    listIndex = listIndex + 1;
+    print ("Institution n details");
+    print (listIndex);
+  }
   @override
   Widget build(BuildContext context) // every widget is a Dart class with build method that returns widgets
   {
+    List<String> titles = ["List1", "List2", "List3"];
     List<String> names = ["Institution 1", "Institution 2", "Institution 3"];
     return new MaterialApp(home: new Scaffold(
       appBar: new AppBar(title: new Text("Oxygen cylinder services"),),
       body: new Column(children: [
-        Text("List of institutions"),
-        RaisedButton(child: new Text("Institution 1"), onPressed: null,),
-        RaisedButton(child: new Text("Institution 2"), onPressed: null,),
-        RaisedButton(child: new Text("Institution 3"), onPressed: null,),
+        Text(titles[listIndex]),
+        RaisedButton(child: new Text(names.elementAt(0)), onPressed: viewDetails,),
+        RaisedButton(child: new Text(names.elementAt(1)), onPressed: (){print ("Institution 2 details");},), // using anonymous function
+        RaisedButton(child: new Text(names[2]), onPressed: (){print ("Institution 3 details");},), // using anonymous function
       ],),
     ),); // scaffold is another widget in material.dart package to create base page design
   }
