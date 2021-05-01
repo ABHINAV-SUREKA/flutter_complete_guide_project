85f459bd80d797a4b6031f88ca414bf546ea1196
abhsurek <abhsurek@cisco.com>
2021-05-01 15:13:31 +0530

    test

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main()
 }
 // above main() function with only one line of code could also be written as: void main() => runApp(new OxyApp());
 
-class OxyApp extends StatelessWidget //  every widget has to extend either of the two: StatelessWidget or StatefulWidget
+class OxyApp extends StatelessWidget // every widget has to extend either of the two abstract classes: StatelessWidget or StatefulWidget
 {
   int listIndex = 0;
   void viewDetails() {
@@ -16,18 +16,33 @@ class OxyApp extends StatelessWidget //  every widget has to extend either of th
     print (listIndex);
   }
   @override
-  Widget build(BuildContext context) // every widget is a Dart class with build method that returns widgets
+  Widget build(BuildContext context) // every widget is a Dart class with build method that returns Widget
   {
     List<String> titles = ["List1", "List2", "List3"];
     List<String> names = ["Institution 1", "Institution 2", "Institution 3"];
-    return new MaterialApp(home: new Scaffold(
-      appBar: new AppBar(title: new Text("Oxygen cylinder services"),),
-      body: new Column(children: [
-        Text(titles[listIndex]),
-        RaisedButton(child: new Text(names.elementAt(0)), onPressed: viewDetails,),
-        RaisedButton(child: new Text(names.elementAt(1)), onPressed: (){print ("Institution 2 details");},), // using anonymous function
-        RaisedButton(child: new Text(names[2]), onPressed: (){print ("Institution 3 details");},), // using anonymous function
-      ],),
-    ),); // scaffold is another widget in material.dart package to create base page design
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Oxygen cylinder services"),
+        ),
+        body: new Column(
+          children: [
+            new Text(titles[listIndex]),
+            new RaisedButton(
+              child: new Text(names.elementAt(0)),
+              onPressed: viewDetails, // writing viewDetails() instead will return void to onPressed and not the (pointer to) function viewDetails itself
+            ),
+            new RaisedButton(
+              child: new Text(names.elementAt(1)),
+              onPressed: () => print ("Institution 2 details"),
+            ), // using anonymous function
+            new RaisedButton(
+              child: new Text(names[2]),
+              onPressed: (){print ("Institution 3 details");},
+            ), // using anonymous function
+          ],
+        ),
+      ),
+    ); // scaffold is another widget in material.dart package to create base page design
   }
 }
