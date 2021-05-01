8fc3fc6d1cc903f5c357976ea6708120e259ff48
abhsurek <abhsurek@cisco.com>
2021-05-01 17:45:20 +0530

    test

--- lib/services_list.dart
+++ lib/services_list.dart
@@ -10,7 +10,7 @@ class ServicesList extends StatelessWidget {
   Widget build (BuildContext context) { // gets rebuild when setState() forces the affected ServicesList widget (through the external ServicesList() constructor call) to re-render (in main.dart)
     return new Container(
       width: double.infinity, // ensures container takes as much size (width) as possible
-      margin: EdgeInsets.all(10),
+      margin: EdgeInsets.all(10), // Dart allows creation of multiple special constructors (eg: here, EdgeInsets.all(), EdgeInsets.only())
       child: new Text(
         servicesListText,
         style: new TextStyle(
